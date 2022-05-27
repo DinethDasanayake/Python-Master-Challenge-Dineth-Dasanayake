a803fa1a7980da2a096537d04a3d0cc33a136a5c
DinethDasanayake <87159798+DinethDasanayake@users.noreply.github.com>
2022-05-27 09:11:11 +0530

    initial commit

---
+++ starter #12 calculate following using a program with any inputs.py
@@ -0,0 +1,27 @@
+'''
+20 points for each question.
+Calculate following using a program with any inputs.
+x + y * z
+y/x - 3 * x
+z - y / x
+2y+3z - z
+n * m * o
+(m-n+o)*p
+4p-3m+2n * 3m
+x*x*x
+-9x+3y-4z
+-9x*-3y
+'''
+
+x = float(input("Enter x :"))
+y = float(input("Enter y :"))
+z = float(input("Enter z :"))
+
+print("x+y+z = ", x+y+z)
+print("y/x - 3 * x = ", y/x - 3*x)
+print("z - y / x = ", z - y / x)
+print("2y+3z - z = ", 2*y + 3*z - z)
+print("x*x*x = ", x*x*x)
+print("-9x+3y-4z = ", -9*x + 3*y - 4*z)
+print("-9x*-3y = ", -9 * x*-3*y)
+print("s"))
