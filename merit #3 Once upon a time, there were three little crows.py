f5c248993bdf882534d4beeee9e6ba46241a93e7
DinethDasanayake <87159798+DinethDasanayake@users.noreply.github.com>
2022-05-27 09:13:06 +0530

    initial commit

---
+++ merit #3 Once upon a time, there were three little crows.py
@@ -0,0 +1,16 @@
+'''
+40 points
+
+Once upon a time, there were three little crows.
+They were Lady Crow, Lord Crow, and Sister
+Crow. Lady Crow was 5 years old. Lord Crow was
+4 years old and Sister Crow was 3 years old.
+What was their total age in two years?
+'''
+
+lady = 5
+lord = lady+4
+sis = lady-3
+
+total = lady+lord+sis
+print(" 4.total age = ", int(total))
