f931d108854e61b136c0b11c59adc14357643973
DinethDasanayake <87159798+DinethDasanayake@users.noreply.github.com>
2022-05-27 08:59:26 +0530

    initial commit

---
+++ starter #11.2 input two numbers and print their substraction.py
@@ -0,0 +1,12 @@
+'''
+20 POINTS
+
+11.0 Input two Numbers and print their :
+11.2 Subtraction
+'''
+
+n1 = int(input("Enter number 1 :"))
+n2 = int(input("Enter number 2 :"))
+
+substract = n1-n2  # Subtraction
+print("Subtraction", substract)
