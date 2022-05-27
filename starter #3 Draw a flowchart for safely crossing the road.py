6d3a4ed2174eb49a74cad52b5af836049a61b877
DinethDasanayake <87159798+DinethDasanayake@users.noreply.github.com>
2022-05-27 08:41:53 +0530

    initial commit

---
+++ starter #3 Draw a flowchart for safely crossing the road.py
@@ -0,0 +1,22 @@
+print("First, go infront of a pedesstrian crossing.")
+print("Then, Look at the right side.")
+v = 1
+
+while v == 1:
+    print("1. Yes")
+    print("2. No")
+    v = int(input("Are vehicles coming? 1 or 2 :"))
+
+print("Look at the left side.")
+v=1
+
+while v == 1:
+    v = int(input("Are vehicles coming? 1 or 2 :"))
+
+print("Look at the right side again.")
+v = 1
+
+while v == 1:
+    v = int(input("Are vehicles coming? 1 or 2 :"))
+print("Now, cross the road quickly.")
+
