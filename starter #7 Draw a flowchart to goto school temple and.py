6495175dcf3b6cccd328aaeda27cfc14e071903f
DinethDasanayake <87159798+DinethDasanayake@users.noreply.github.com>
2022-05-27 08:57:28 +0530

    initial commit

---
+++ starter #7 Draw a flowchart to goto school temple and.py
@@ -0,0 +1,19 @@
+print("1. School")
+print("2. Temple")
+print("3. Super Market")
+p=int(input("Where do you want to go (1, 2, 3) ?"))
+
+if p==1:
+    print("Get all the books, lunch and water bottle.")
+    print("Then, wear your school uniform.")
+    print("Finally, go to the school")
+elif p==2:
+    print("Get the flowers, oil, lamps, safety matches etc. ")
+    print("Then, wear a calm white dress.")
+    print("Finally, go to the temple.")
+elif p==3:
+    print("Write the things that you want to buy in a list ")
+    print("Then, take  the list and get enough money")
+    print("Finally, go to the supermarket.")
+else:
+    pass
