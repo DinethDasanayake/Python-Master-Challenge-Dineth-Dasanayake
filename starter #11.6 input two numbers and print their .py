df3981efdccdb6107f2c220b840f802ababa2b9a
DinethDasanayake <87159798+DinethDasanayake@users.noreply.github.com>
2022-05-27 09:03:21 +0530

    initial commit

---
+++ starter #11.6 input two numbers and print their .py
@@ -0,0 +1,13 @@
+'''
+20 POINTS
+
+11.0 Input two Numbers and print their :
+11.6 Division without fraction
+'''
+
+
+n1 = int(input("Enter number 1 :"))
+n2 = int(input("Enter number 2 :"))
+
+divide_without_fraction = int(n1/n2)  # Division without fraction
+print("Division without fraction", divide_without_fraction)
