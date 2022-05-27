ed404d7d8cb8be804fc122afd7fa1257d0f0fa04
DinethDasanayake <87159798+DinethDasanayake@users.noreply.github.com>
2022-05-27 09:23:53 +0530

    initial commit

---
+++ advanced merit #3 Display numbers in descending order.py
@@ -0,0 +1,22 @@
+n1 = input("Enter number 1:")
+n2 = input("Enter number 2:")
+n3 = input("Enter number 3:")
+
+if n1 < n2:
+    if n1 < n3:
+        if n2 < n3:
+            print(n1, ",", n2, ",", n3)
+        else:
+            print(n1, ",", n3, ",", n2)
+    else:
+        print(n3, ",", n1, ",", n2)
+elif n2 < n3:
+    if n1 < n3:
+        print(n2, ",", n1, ",", n3)
+    else:
+        print(n2, ",", n3, ",", n1)
+else:
+    if n1 < n2:
+        print(n3, ",", n1, ",", n2)
+    else:
+        print(n3, ",", n2, ",", n1)
