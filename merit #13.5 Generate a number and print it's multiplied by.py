4b409a705fafb88794e843059ae8a84167e82db4
DinethDasanayake <87159798+DinethDasanayake@users.noreply.github.com>
2022-05-27 09:22:09 +0530

    initial commit

---
+++ merit #13.5 Generate a number and print it's multiplied by.py
@@ -0,0 +1,10 @@
+import random
+
+n = random.randint(0, 10)
+i=0
+print("Number=",n)
+
+for i in range(10):
+    print(str(n), "* +1 =", n*+1)
+    print(str(n), "* -1 =", n*-1)
+    print("____________")
