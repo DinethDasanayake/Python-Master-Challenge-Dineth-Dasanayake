b3a280d6c08a7d7d4aef11895da4deab1208d524
DinethDasanayake <87159798+DinethDasanayake@users.noreply.github.com>
2022-05-27 09:13:22 +0530

    initial commit

---
+++ merit #4 One day, each little crow wanted to build a nest.py
@@ -0,0 +1,14 @@
+'''
+One day, each little crow wanted to build a nest.
+Lady Crow wanted to build a nest of straw. Straw
+costs Rs.4 a bundle. She needs 9 bundles. How
+much will she get as a balance if she pays the
+shop with a Rs.100 note?
+'''
+s_cost=4
+bundles=9
+total=s_cost*bundles
+
+payment=100
+balance=payment-total
+print("Balance is Rs.",balance)
