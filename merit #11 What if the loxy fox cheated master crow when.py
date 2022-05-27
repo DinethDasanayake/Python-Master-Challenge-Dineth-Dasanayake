af2a04cbaba8fb6b9bb8aa15aab37ab474cb6e6f
DinethDasanayake <87159798+DinethDasanayake@users.noreply.github.com>
2022-05-27 09:16:40 +0530

    initial commit

---
+++ merit #11 What if the loxy fox cheated master crow when.py
@@ -0,0 +1,6 @@
+cheat_cm = 80 #centimeters cheated for 1 meter
+cheat_m = cheat_cm/100 #meters cheated for 1 meter
+
+buying_cm = 800
+total_cheat = cheat_m*buying_cm
+print("Total centimeters fox cheated =",total_cheat,"centimeters")
