97bb7234cb0572b6c13b3712975d64815dce679d
DinethDasanayake <87159798+DinethDasanayake@users.noreply.github.com>
2022-05-27 09:12:07 +0530

    initial commit

---
+++ merit #1 Once upon a time, there were three little crows.py
@@ -0,0 +1,14 @@
+'''
+40 points
+
+Once upon a time, there were three little crows
+with ages 2, 4, and 6 years. What is the total of
+their ages?
+'''
+
+c1 = 2
+c2 = 4
+c3 = 6
+
+total = c1+c2+c3
+print(" 2.total of their ages = ", total)
