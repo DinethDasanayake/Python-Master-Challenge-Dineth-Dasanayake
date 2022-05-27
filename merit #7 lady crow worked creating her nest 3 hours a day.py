f898512814ae6f006db87282e707990f07ffe467
DinethDasanayake <87159798+DinethDasanayake@users.noreply.github.com>
2022-05-27 09:14:32 +0530

    initial commit

---
+++ merit #7 lady crow worked creating her nest 3 hours a day.py
@@ -0,0 +1,13 @@
+n1 = int(input("Enter number 1 :"))
+n2 = int(input("Enter number 2 :"))
+n3 = int(input("Enter number 3 :"))
+
+if n1 > n2:
+    if n1 > n3:
+        print(n1, " is the largest")
+if n2 > n1:
+    if n2 > n3:
+        print(n2, "is the largest")
+if n3 > n1:
+    if n3 > n2:
+        print(n3, "is the largest")
