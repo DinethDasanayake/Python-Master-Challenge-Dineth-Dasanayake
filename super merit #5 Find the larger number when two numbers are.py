416c1b5272c8ae567f4d5cad2f186c5b13576399
DinethDasanayake <87159798+DinethDasanayake@users.noreply.github.com>
2022-05-27 09:39:18 +0530

    initial commit

---
+++ super merit #5 Find the larger number when two numbers are.py
@@ -0,0 +1,9 @@
+n1 = int(input("Enter number 1 :"))
+n2 = int(input("Enter number 2 :"))
+
+if n1 > n2:
+    print("number 1 is larger")
+elif n2 > n1:
+    print("number 2 is larger")
+else:
+    print("Both are equal")
