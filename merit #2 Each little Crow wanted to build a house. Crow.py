94d24c8751f3a6c177bb5191f5396a75a90dd243
DinethDasanayake <87159798+DinethDasanayake@users.noreply.github.com>
2022-05-27 09:12:33 +0530

    initial commit

---
+++ merit #2 Each little Crow wanted to build a house. Crow.py
@@ -0,0 +1,14 @@
+'''
+40 points
+
+2. Each little Crow wanted to build a house. Crow #1
+wanted to build a house of straw. Straw costs Rs.4
+a bundle. He needs 9 bundles. He has Rs. 4. How
+much more?
+'''
+
+straw_cost = 4
+bundles = 9
+
+total = straw_cost*bundles
+print(" 3.cost he spend = ", total)
