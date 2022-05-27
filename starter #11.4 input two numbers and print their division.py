de7c152063435fc0164e68be3243eeae312fdcd4
DinethDasanayake <87159798+DinethDasanayake@users.noreply.github.com>
2022-05-27 09:00:09 +0530

    initial commit

---
+++ starter #11.4 input two numbers and print their division.py
@@ -0,0 +1,13 @@
+'''
+20 POINTS
+
+11.0 Input two Numbers and print their :
+11.4 Division
+'''
+
+n1 = int(input("Enter number 1 :"))
+n2 = int(input("Enter number 2 :"))
+
+divide = n1/n2  # Division
+print("Division", divide)
+
