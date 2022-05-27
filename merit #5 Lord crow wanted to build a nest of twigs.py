fe2b2a6b875f6e73412cf09fa5fa98259d164d4b
DinethDasanayake <87159798+DinethDasanayake@users.noreply.github.com>
2022-05-27 09:13:42 +0530

    initial commit

---
+++ merit #5 Lord crow wanted to build a nest of twigs.py
@@ -0,0 +1,14 @@
+'''
+40 points
+
+. Lord crow wanted to build a nest of twigs. Each
+bundle of twigs weighs 50 grams. Lord crow
+needs 12 bundles. How much will they weigh in
+Kilograms ?
+'''
+
+weight = 50
+bundles = 12
+
+total = weight*bundles
+print(" 5.total weight = ", total)
