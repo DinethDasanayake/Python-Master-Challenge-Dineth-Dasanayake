676b0b869ca039e1135dd7718a9395ff10ed0980
DinethDasanayake <87159798+DinethDasanayake@users.noreply.github.com>
2022-05-27 09:38:39 +0530

    initial commit

---
+++ super merit #3 Combine above two programs into one program giving.py
@@ -0,0 +1,21 @@
+print("1. Celsius to Fahrenheit")
+print("2. Fahrenheit to Celsius")
+print("3. Exit")
+#con = conversion
+con = int(input("What do you want (type 1/2/3) :"))
+
+if con == 1:
+    c = float(input("Enter the temperature in Celcius :"))
+    f = (9/5*c)+32
+    print(c, "°C =", f, "°F")
+
+elif con == 2:
+    f = int(input("Enter the temperature in Fahrenheit :"))
+    c = int(5/9 * (f - 32))
+    print(f, "°F =", c, "°C")
+print(int )while foi r in ran ge rang:e in it 
+elif con == 3:
+    pass
+
+else :
+    print("Please enter a vaild number")
