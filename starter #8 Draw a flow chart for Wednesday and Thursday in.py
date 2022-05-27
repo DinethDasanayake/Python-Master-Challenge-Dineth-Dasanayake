fd6e841adeb34f7ef8595c5acd15e8b8f362a98e
DinethDasanayake <87159798+DinethDasanayake@users.noreply.github.com>
2022-05-27 08:58:07 +0530

    initial commit

---
+++ starter #8 Draw a flow chart for Wednesday and Thursday in.py
@@ -0,0 +1,28 @@
+d = str(input("What is the day? Tuesday or Wednesday :"))
+if d == "Tuesday":
+    print("\n Tuesday Time Table ")
+    print("\n 1st period - Sinhala")
+    print("2st period - Maths")
+    print("3st period - English")
+    print("4st period - Health Science")
+    print("5st period - Geography")
+    print("interval")
+    print("6st period - Science")
+    print("7st period - Aesthetics")
+    print("8st period - Aesthetics")
+
+elif d == "Wednesday":
+    print("\n Time Table ")
+    print("\n Tuesday Time Table ")
+    print("\n 1st period - Sinhala")
+    print("2st period - History")
+    print("3st period - Civic Education")
+    print("4st period - Tamil")
+    print("5st period - Buddhism")
+    print("interval")
+    print("6st period - Maths")
+    print("7st period - English")
+    print("8st period - Science")
+
+else:
+    print("Please enter the day as Tuesday or Wednesday")
