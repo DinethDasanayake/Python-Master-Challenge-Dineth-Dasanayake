154c587861c524bce32774fc77c450ef505f343c
DinethDasanayake <87159798+DinethDasanayake@users.noreply.github.com>
2022-05-27 09:03:02 +0530

    initial commit

---
+++ starter #11.5 input two numbers and print their remainder of the division.py
@@ -0,0 +1,13 @@
+'''
+20 POINTS
+
+11.0 Input two Numbers and print their :
+11.5 Remainder of the division
+'''
+
+
+n1 = int(input("Enter number 1 :"))
+n2 = int(input("Enter number 2 :"))
+
+remainder = n1 % n2  # Remainder of the division
+print("Remainder of the division", remainder)
