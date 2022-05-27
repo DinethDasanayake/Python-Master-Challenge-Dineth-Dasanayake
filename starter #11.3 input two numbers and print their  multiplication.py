d1249ee1a28d8120386686fe64461fc50a88acd9
DinethDasanayake <87159798+DinethDasanayake@users.noreply.github.com>
2022-05-27 08:59:43 +0530

    initial commit

---
+++ starter #11.3 input two numbers and print their  multiplication.py
@@ -0,0 +1,13 @@
+'''
+20 POINTS
+
+11.0 Input two Numbers and print their :
+11.3 Multiplication
+'''
+
+n1 = int(input("Enter number 1 :"))
+n2 = int(input("Enter number 2 :"))
+
+multiply = n1*n2  # Multiplication
+print("Multiplication", multiply)
+
