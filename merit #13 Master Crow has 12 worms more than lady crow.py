80fae1a106ed6547ab671444e13765fa759dcff7
DinethDasanayake <87159798+DinethDasanayake@users.noreply.github.com>
2022-05-27 09:19:20 +0530

    initial commit

---
+++ merit #13 Master Crow has 12 worms more than lady crow.py
@@ -0,0 +1,10 @@
+n = int(input("Enter number of warms :"))
+
+master_worm = int(n)
+daughter_worm = int(master_worm/2)
+lady_worm = int(daughter_worm+6)
+
+
+print("Master crow has", master_worm, "worms")
+print("Lady crow has", lady_worm, "worms")
+print("Daughter crow has", daughter_worm, "worms")
