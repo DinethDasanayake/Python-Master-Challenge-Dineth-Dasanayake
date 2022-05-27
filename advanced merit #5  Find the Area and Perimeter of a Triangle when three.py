819c733c8302ddea3324a7526fed0b52296edd7f
DinethDasanayake <87159798+DinethDasanayake@users.noreply.github.com>
2022-05-27 09:24:50 +0530

    initial commit

---
+++ advanced merit #5  Find the Area and Perimeter of a Triangle when three.py
@@ -0,0 +1,12 @@
+l1 = int(input("Enter the length of first side :"))
+l2 = int(input("Enter the length of second side :"))
+l3 = int(input("Enter the length of third side :"))
+
+b = int(input("Enter the base of the triangle:"))
+h = int(input("Enter the perpendicular height of the triangle:"))
+
+perimeter = l1+l2+l3
+area = 1/2*b*h
+
+print("Perimeter =", perimeter)
+print("Area =", area)
