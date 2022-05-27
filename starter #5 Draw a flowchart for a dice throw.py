5d06aedd505f9d938add18944973bb0b34558e88
DinethDasanayake <87159798+DinethDasanayake@users.noreply.github.com>
2022-05-27 08:45:29 +0530

    initial commit

---
+++ starter #5 Draw a flowchart for a dice throw.py
@@ -0,0 +1,18 @@
+print("Throw the dice.")
+n = int(input("Enter the value you get :"))
+
+if n == 1:
+    print("Move 1 step forward.")
+elif n == 2:
+    print("Move 2 steps forward.")
+elif n == 3:
+    print("Move 3 steps forward.")
+elif n == 4:
+    print("Move 4 steps forward.")
+elif n == 5:
+    print("Move 5 steps forward.")
+elif n == 6:
+    print("Move 6 steps forward.")
+else:
+    print("Please enter a vaild value")
+
