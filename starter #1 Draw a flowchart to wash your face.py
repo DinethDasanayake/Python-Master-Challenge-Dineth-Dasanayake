05f74956aa83a30198288e99f74b94f6f02069f8
DinethDasanayake <87159798+DinethDasanayake@users.noreply.github.com>
2022-05-27 08:40:28 +0530

    initial commit

---
+++ starter #1 Draw a flowchart to wash your face.py
@@ -0,0 +1,9 @@
+print("First, open the tap")
+print("Then,wash the face well.")
+print("After that,close the tap.")
+print("Then,apply some soap.")
+print("Open the tap again.")
+print("wash the face again until no soap is left.")
+print("Then, close the tap.")
+print("Finally, wipe the water with a towel")
+
