80906f37dd19d8a0f5c89d919baf9dc19c12d65d
DinethDasanayake <87159798+DinethDasanayake@users.noreply.github.com>
2022-05-27 09:19:59 +0530

    initial commit

---
+++ merit #13.2  generate 10 random numbers and print their sum.py
@@ -0,0 +1,10 @@
+import random
+sum = 0
+for i in range(10):
+    i = random.randint(0, 10)
+    print(i)
+    sum += i #sum
+sum_muti =sum * 2 #sum multiplied by 2
+
+print("sum =", sum)
+print("sum multiplied by 2 =", sum_muti)
