545fc1bda200e26500a496246de9f35bd36fda18
DinethDasanayake <87159798+DinethDasanayake@users.noreply.github.com>
2022-05-27 08:42:11 +0530

    initial commit

---
+++ starter #4 Draw a flowchart to make a cup of tea.py
@@ -0,0 +1,14 @@
+print("1. First take tea,cup,sugar,jug and some water.")
+print("2. Then,boil the water.")
+print("3. Then,boil the water.")p
+for t in range(100):
+    t += 1
+    print(t)
+
+print("4. Now water has been boiled")
+print("5. Put 3/4 tea spoon of tea into the jug")
+print("6. Pour about 500 ml water into th jug")
+print("7. Pour them into the cup.")
+print("8. Add about 3 spoons of sugar into the cup")
+print("9. Mix it well.")
+print("10. Now, your tea is ready to drink")
