82d0a2ddef2132a938e73fd5377d1d4baf407096
DinethDasanayake <87159798+DinethDasanayake@users.noreply.github.com>
2022-05-27 09:21:07 +0530

    initial commit

---
+++ merit #13.4  Generate 5 random numbers and print squre of.py
@@ -0,0 +1,25 @@
+import random
+
+n1 = random.randint(0, 10)
+n2 = random.randint(0, 10)
+n3 = random.randint(0, 10)
+n4 = random.randint(0, 10)
+n5 = random.randint(0, 10)
+
+print("number 1=", n1)
+print("number 2=", n2)
+print("number 3=", n3)
+print("number 4=", n4)
+print("number 5=", n5)
+
+sq1 = n1**2
+sq2 = n2**2
+sq3 = n3**2
+sq4 = n4**2
+sq5 = n5**2
+
+print("number 1 :",str(sq1), "-", str(n1), "=", sq1-n1)
+print("number 2 :",str(sq2), "-", str(n2), "=", sq2-n2)
+print("number 3 :",str(sq3), "-", str(n3), "=", sq3-n3)
+print("number 4 :",str(sq4), "-", str(n4), "=", sq4-n4)
+print("number 5 :",str(sq5), "-", str(n5), "=", sq5-n5)
